39e2a3204d1e5b2902d152cca8b2afa2355178c3
jbonaiuto <jbonaiuto@gmail.com>
2023-09-08 12:52:49 +0200

    updated matlab version to match latest python version

--- matlab/ar_surr.m
+++ matlab/ar_surr.m
@@ -1,39 +1,25 @@
 function amp_prods=ar_surr(signal, n_shuffles)
 
-    n_trials = size(signal,1);
-    n_pts = size(signal,2);
+    n_trials = size(signal, 1);
+    n_pts = size(signal, 2);
 
-    amp_prods=zeros(n_shuffles*n_trials,1);
-    pad=zeros(1,n_pts);
+    amp_prods = zeros(n_trials, n_shuffles, n_pts-1);
 
-    for i=1:n_trials
-        % Subtract out the mean and linear trend
-        detrend_ord = 1;
-        x=detrend(signal(i,:), detrend_ord);
-        
+    parfor i = 1:n_trials
+        x = signal(i, :);
+
         % Estimate an AR model
-        mdl = arima(1,0,0);
+        mdl = arima(1, 0, 0);
         mdl = estimate(mdl, x', 'Display', 'off');
 
-        x_sim = simulate(mdl,n_pts,'NumPaths',1000);
+        x_sim = simulate(mdl, n_pts, 'NumPaths', n_shuffles)';
 
-        % Subtract out the mean and linear trend
-        x_sim=detrend(x_sim, detrend_ord)';
-        
-        for j=1:n_shuffles
-            padd_rand_signal = [pad, x_sim(j,:), pad];
-            % Get analytic signal (phase and amplitude)
-            analytic_rand_signal = hilbert(padd_rand_signal);
-            % Cut off padding
-            analytic_rand_signal=analytic_rand_signal(n_pts+1:2 * n_pts);
+        padd_rand_signal = [zeros(size(x_sim)), x_sim, zeros(size(x_sim))];
+        analytic_rand_signal = hilbert(padd_rand_signal')';
+        analytic_rand_signal = analytic_rand_signal(:, n_pts+1:2*n_pts);
 
-            % Analytic signal at n=0...-1
-            f1 = analytic_rand_signal(1:end-1);
+        f1 = analytic_rand_signal(:, 1:end-1);
+        f2 = analytic_rand_signal(:, 2:end);
+        amp_prods(i, :, :) = abs(f1) .* abs(f2);
 
-            % Analytic signal at n=1...
-            f2 = analytic_rand_signal(2:end);
-
-            amp_prod = abs(f1) .* abs(f2);
-            amp_prods((i-1)*n_shuffles+j)=mean(amp_prod);
-        end
     end
