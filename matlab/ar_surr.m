039dcfd39dc0f36dfa8c65e9a1c32bface382066
jbonaiuto <jbonaiuto@gmail.com>
2023-10-05 13:25:58 +0200

    removed dependence on econometrics toolbox

--- matlab/ar_surr.m
+++ matlab/ar_surr.m
@@ -9,11 +9,38 @@ function amp_prods=ar_surr(signal, n_shuffles)
         x = signal(i, :);
 
         % Estimate an AR model
-        mdl = arima(1, 0, 0);
-        mdl = estimate(mdl, x', 'Display', 'off');
+        %mdl = arima(1, 0, 0);
+        %mdl = estimate(mdl, x', 'Display', 'off');
+        p = 1; % Order of AR model
 
-        x_sim = simulate(mdl, n_pts, 'NumPaths', n_shuffles)';
+        % Compute mean-adjusted data
+        dataMeanAdjusted = x - mean(x);
 
+        % Compute autocovariance
+        autoCov = xcov(dataMeanAdjusted, p, 'biased');
+
+        % Formulate the Yule-Walker equations
+        R = toeplitz(autoCov(p+1:p+p));
+        rho = autoCov(p+2:p+p+1);
+
+        % Solve the Yule-Walker equations to get AR coefficients
+        arCoeff = R\rho;
+
+        %x_sim = simulate(mdl, n_pts, 'NumPaths', n_shuffles)';
+        x_sim = zeros(n_shuffles, n_pts);
+        for j = 1:n_shuffles
+            % Initial values (you can replace these with specific initial values if desired)
+            x_sim(j,1:p) = x(1:p);
+
+            % Generate AR simulated data
+            for t = p+1:n_pts
+                x_sim(j,t) = -arCoeff' * x_sim(j,t-p:t-1) + randn;
+            end
+
+            % If the mean was adjusted, add it back to the simulated data
+            x_sim(j,:) = x_sim(j,:) + mean(x);
+        end
+        
         padd_rand_signal = [zeros(size(x_sim)), x_sim, zeros(size(x_sim))];
         analytic_rand_signal = hilbert(padd_rand_signal')';
         analytic_rand_signal = analytic_rand_signal(:, n_pts+1:2*n_pts);
